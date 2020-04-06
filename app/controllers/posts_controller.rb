6609e7a0552cf0790112c2be0f471439ccc478a7
Carla Holcomb <carla.programmer@gmail.com>
2020-04-06 23:15:41 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,7 @@ class PostsController < ApplicationController
 
   def update
     
-    if @post.update
+    if @post.update(post_params)
       @post.save
       redirect_to post_path(@article)
     else
