59d575fb237725605f9a58268ae6540bf0badd67
Carla Holcomb <carla.programmer@gmail.com>
2020-04-06 23:15:24 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,22 +8,11 @@ class PostsController < ApplicationController
   end
 
   def update
-    # Create a brand new, unsaved, not-yet-validated Post object from the form.
-    @post = Post.new(post_params)
- 
-    # Run the validations WITHOUT attempting to save to the database, returning
-    # true if the Post is valid, and false if it's not.
-    if @post.valid?
-      # If--and only if--the post is valid, do what we usually do.
+    
+    if @post.update
       @post.save
-      # This returns a status_code of 302, which instructs the browser to
-      # perform a NEW REQUEST! (AKA: throw @post away and let the show action
-      # worry about re-reading it from the database)
       redirect_to post_path(@article)
     else
-      # If the post is invalid, hold on to @post, because it is now full of
-      # useful error messages, and re-render the :new page, which knows how
-      # to display them alongside the user's entries.
       render :edit
     end
   end
